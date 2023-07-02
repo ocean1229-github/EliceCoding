5b18d1f6c469d10dcd521ffe557b515a25325da4
DoohyenKang <62789182+ocean1229-github@users.noreply.github.com>
2023-07-02 22:26:13 +0900

    arrow function

---
+++ Day_3/arrow_function.js
@@ -0,0 +1,12 @@
+// function add ...로 선언된 add 함수를 arrow function 표현법으로 재작성해 봅니다.
+
+// arrow function을 사용하여 함수를 선언합니다.
+// 함수는 const add로 선언합니다.
+// 주어진 함수와 동일한 동작을 하도록 수정합니다
+
+/* 지시사항의 내용을 따라 아래 코드를 수정해보세요. */
+const add = (a, b) => {
+    return `${a}+${b}=${a+b}`;
+}
+
+module.exports = add;

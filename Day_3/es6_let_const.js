e72324bf4972b69db34ce3109e1d8a9132be42c9
DoohyenKang <62789182+ocean1229-github@users.noreply.github.com>
2023-07-02 22:17:07 +0900

    ES6 let, const 사용하기

---
+++ Day_3/es6_let_const.js
@@ -0,0 +1,11 @@
+// variable은 언제든 변경이 가능한 변수로,
+// constant는 변경이 불가능한 상수로 사용려고 합니다.
+
+// 각각의 선언문을 let과 const를 적절히 활용하여 수정해 봅시다.
+
+/* 지시사항의 내용을 따라 아래 코드를 수정해보세요. */
+let variable = 1;
+const constant = 2;
+
+console.log('variable', 1);
+console.log('constant', 2);

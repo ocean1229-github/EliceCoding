46fef7651128d28ac2351b6c801ceff40890c552
DoohyenKang <62789182+ocean1229-github@users.noreply.github.com>
2023-07-11 22:34:02 +0900

    path parameter 사용하기

--- Day_9/path_param/index.js
+++ Day_9/path_param/index.js
@@ -1,3 +1,16 @@
+// 다음은 Express.js로 구현한 간단한 서버 프로그램입니다.
+
+// 지시사항에 따라 path parameter를 사용해 봅니다.
+
+
+// 지시사항
+// path parameter를 사용하여 메시지를 응답으로 보내는 라우팅을 완료합니다.
+
+// /say/ 뒤에 어떤 문자열이 오든지 해당 문자열을 응답으로 출력합니다.
+
+// ex)
+// /say/hi라는 요청이 들어오면 응답으로 “hi” 가 출력됩니다.
+
 const express = require('express');
 
 const app = express();
